368964b40f8396488488d9440cd570c58cac96ed
Shadeisroot <dschristensen@proton.me>
2026-07-23 02:32:56 +0200

    Remove create_tables.py as table creation is no longer needed

--- create_tables.py
+++
@@ -1,6 +0,0 @@
-from app.database import Base, engine
-from app.models.doorcode import DoorCode
-from app.models.user import User   # ← add
-
-Base.metadata.create_all(bind=engine)
-print("Tables created!")
